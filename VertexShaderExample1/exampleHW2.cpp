ce18f9237d37c53d92ae1e9139b84abd2db38c4d
DavidTu <a3102a123@gmail.com>
2021-11-20 15:27:31 +0800

    add the explanation of sample code

--- VertexShaderExample1/exampleHW2.cpp
+++ VertexShaderExample1/exampleHW2.cpp
@@ -22,20 +22,11 @@ void bindBuffer();
 
 //Number of vertices that will be sent to shader
 int verticeNumber = 3;
-
-//Storing vertex datas that will be sent to shader
-class VertexAttribute {
-public:
-	GLfloat position[3];
-	void setPosition(float x, float y, float z) {
-		position[0] = x;
-		position[1] = y;
-		position[2] = z;
-	};
-};
+int verticeSize = 3;
 
 //Collecting vertex datas
-VertexAttribute *drawTriangle();
+float *drawTriangle();
+void setPosition(float* vertexArr, float x, float y, float z);
 
 int main(int argc, char** argv) {	
 	glutInit(&argc, argv);
@@ -62,16 +53,22 @@ void shaderInit() {
 }
 
 void bindBuffer() {
-	//Generate a new buffer object
+	// Just a example shows how to buffer data.
+	// You need to use VAO + VBO in main.cpp to buffer 
+	
+	// Generate a new buffer object
 	glGenBuffers(1, &vboName);
 	glBindBuffer(GL_ARRAY_BUFFER, vboName);
 
 	//Copy vertex data to the buffer object
-	VertexAttribute* vertices;
+	float* vertices;
 	vertices = drawTriangle();
-	glBufferData(GL_ARRAY_BUFFER, sizeof(VertexAttribute) * verticeNumber, vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * verticeNumber * verticeSize, vertices, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(VertexAttribute), (void*)(offsetof(VertexAttribute, position)));
+	// here using the sample array so the last parameter is 0 which means the offset of the fist component in buffer.
+	// It can let Vertex Attribute pointer know where to start load data in buffer
+	// you can use the function : "offsetof(ClassName, ComponentName)" to set up last parameter when using the class or stuct.
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)(0));
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
@@ -101,31 +98,45 @@ void display() {
 
 	GLfloat pmtx[16];
 	GLfloat mmtx[16];
+	// get current modelview & projection matrix used in OpenGL rendering
 	glGetFloatv(GL_PROJECTION_MATRIX, pmtx);
 	glGetFloatv(GL_MODELVIEW_MATRIX, mmtx);
+
+	// get the "location" (It's similar to pointer) of uniform variable in shader
  	GLint pmatLoc = glGetUniformLocation(program, "Projection");
 	GLint mmatLoc = glGetUniformLocation(program, "ModelView");
 
+	// using shader
 	glUseProgram(program);
 
-	//input the modelview matrix into vertex shader
+	// pass the projection matrix into vertex shader
 	glUniformMatrix4fv(pmatLoc, 1, GL_FALSE, pmtx);
-	//input the rotation matrix into vertex shader
+	// pass the modelview matrix into vertex shader
 	glUniformMatrix4fv(mmatLoc, 1, GL_FALSE, mmtx);
 
+	// draw the vertex stored in buffer 
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 8);
 	glUseProgram(0);
 	glPopMatrix();
 	glutSwapBuffers();
 }
 
-VertexAttribute *drawTriangle() {
-	VertexAttribute *vertices;
-	vertices = new VertexAttribute[verticeNumber];
-	vertices[0].setPosition(0,0,0);
-	vertices[1].setPosition(1,0,0);
-	vertices[2].setPosition(0,1,0);
+// generate the vertex infomation of model (here is a simple triangle)
+// you may need to contain more infomation (such like : texture coordinate).
+// using VertexAttribute class to store correlated data defined in Vertex.h.
+float *drawTriangle() {
+	float*vertices;
+	vertices = new float[verticeNumber * verticeSize];
+	setPosition(vertices, 0, 0, 0);
+	setPosition(vertices + verticeSize, 1, 0, 0);
+	setPosition(vertices + 2 * verticeSize, 0, 1, 0);
 	return vertices;
+}
+
+void setPosition(float * vertexArr,float x,float y,float z) {
+	*vertexArr = x;
+	*(vertexArr + 1) = y;
+	*(vertexArr + 2) = z;
 }
 
 void reshape(GLsizei w, GLsizei h) {
